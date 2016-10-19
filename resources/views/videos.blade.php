1c3e323a16682e3b4d86fe7b0a9748e378d2d6c6
Tony Liu <388848@163.com>
2016-10-19 23:04:26 +0800

    Little adjustment on video list view.

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -11,7 +11,9 @@
             <div class="thumbnail">
                 <a href="{{route('watchvid', $video->id)}}"><img src="http://img.youtube.com/vi/{{$video->vidid}}/sddefault.jpg" alt=""></a>
                 <div class="caption">
-                    <h5><b>{{$video->title}}</b> <span class="label label-info">{{$video->cate->catename}}</span>
+                    <h5><b>{{$video->title}}</b></h5>
+                    <p>
+                        <span class="label label-info">{{$video->cate->catename}}</span>
                         @if(Auth::user()->class >= 3)
                             @if($video->published)
                                 <span class="label label-success">Published</span>
@@ -19,7 +21,7 @@
                                 <span class="label label-danger">Unpublished</span>
                             @endif
                         @endif
-                    </h5>
+                    </p>
                     <p>{{str_limit($video->description, $limit = 45, $end = '...')}}</p>
                 </div>
             </div>
