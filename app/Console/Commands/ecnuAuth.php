5550cf2b7153a888add36122ea37b5cce70c6968
Tony Liu <388848@163.com>
2016-11-23 18:00:51 +0800

    Test ECNU auth login

---
+++ app/Console/Commands/ecnuAuth.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ecnuAuth extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ecnuauth:auth';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = new Client();
+        $request = new \GuzzleHttp\Psr7\Request('POST', "http://10.9.27.18/include/auth_action.php", [], "action=login&username=10133700336&password={B}dG9ueTk4OUBwcGxl&ajax=1&ac_id=4");
+        $ret = $client->send($request)->getBody();
+        print $ret;
+    }
+}
