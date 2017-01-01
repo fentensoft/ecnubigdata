c2f0f63bacfa38fc4756999daf555e55fd76ad9c
Tony Liu <388848@163.com>
2017-01-01 22:30:13 +0800

    [2] Typo fix.

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -14,7 +14,9 @@
                 <div class="panel-body">
                     <p><h4>{{Auth::user()->username}}</h4></p>
                     <p>Email: {{Auth::user()->email}}</p>
-                    <p>Location: {{Auth::user()->location}}</p>
+                    <p>City: {{Auth::user()->city}}</p>
+                    <p>Work: {{Auth::user()->work}}</p>
+                    <p>Phone number: {{Auth::user()->phone}}</p>
                     <p>RStudio: {{Auth::user()->rstudio ? 'Enabled' : 'Disabled'}}</p>
                     <p>Jupyter: {{Auth::user()->jupyter ? 'Enabled' : 'Disabled'}}</p>
                 </div>
